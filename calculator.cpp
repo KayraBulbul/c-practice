f18aad4d885645f3989a32f963ddcad1be04df46
Kayra <kayra19206@gmail.com>
2026-07-14 23:21:46 +1000

    tidy up

--- calculator.cpp
+++ calculator.cpp
@@ -2,39 +2,29 @@
 
 int main() {
   char op;
-  int num1;
-  int num2;
+  double num1;
+  double num2;
 
   while (true) {
     std::cout << "Enter the operator you wish to choose: ";
     std::cin >> op;
+
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
     switch (op) {
     case '+':
-      std::cout << "Enter the first number: ";
-      std::cin >> num1;
-      std::cout << "Enter the second number: ";
-      std::cin >> num2;
       std::cout << num1 + num2;
       break;
     case '-':
-      std::cout << "Enter the first number: ";
-      std::cin >> num1;
-      std::cout << "Enter the second number: ";
-      std::cin >> num2;
       std::cout << num1 - num2;
       break;
     case '*':
-      std::cout << "Enter the first number: ";
-      std::cin >> num1;
-      std::cout << "Enter the second number: ";
-      std::cin >> num2;
       std::cout << num1 * num2;
       break;
     case '/':
-      std::cout << "Enter the first number: ";
-      std::cin >> num1;
-      std::cout << "Enter the second number: ";
-      std::cin >> num2;
       std::cout << num1 / num2;
       break;
     default:
