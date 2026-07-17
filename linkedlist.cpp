b90e38508fc87402278658728ee32b137aa87a2c
Kayra <kayra19206@gmail.com>
2026-07-17 14:01:29 +1000

    Fixed some issues that AI pointed out. Wrote comments for myself to remember these issues later

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -11,7 +11,14 @@ private:
   Node *head = nullptr;
 
 public:
-  LinkedList() {}
+  // LinkedList() {} This can be done like the way below because head already
+  // has a default member initialiser
+  LinkedList() = default;
+  // This prevents copying. Currently if I were to copy without these, it'd be a
+  // shallow copy so they'd all point to the same nodes and both destructors
+  // would try to delete them
+  LinkedList(const LinkedList &) = delete;
+  LinkedList &operator=(const LinkedList &) = delete;
 
   void pushFront(int value) {
     Node *newNode = new Node{value, this->head};
@@ -31,39 +38,64 @@ public:
     return poppedNode;
   }
 
-  int size() {
+  // Because these methods don't change the actual linked list they can be const
+  int size() const {
     int count = 0;
-    Node *current = this->head;
+    const Node *current = this->head;
 
     while (current != nullptr) {
-      count += 1;
+      ++count;
       current = current->next;
     }
     return count;
   }
 
-  void print() {
-    Node *current = this->head;
+  void print() const {
+    // Const is allowed for these because changing the pointer is fine, but
+    // const prevents changing the value or next pointer
+    const Node *current = this->head;
 
     while (current != nullptr) {
       std::cout << current->value << std::endl;
       current = current->next;
     }
   }
+
+  // Destructor to remove the linked list out of memory
+  ~LinkedList() {
+    Node *current = head;
+
+    while (current != nullptr) {
+      Node *nextNode = current->next;
+      delete current;
+      current = nextNode;
+    }
+  }
 };
 
 int main() {
-  LinkedList *list = new LinkedList;
+  // LinkedList *list = new LinkedList; Can be done like the way below, doesn't
+  // require explicit call of the destructor
+  LinkedList list;
 
-  list->pushFront(7);
-  list->pushFront(3);
-  list->pushFront(9);
-  list->pushFront(6);
-  list->pushFront(5);
-  list->pushFront(3);
-  std::cout << "List size: " << list->size() << std::endl;
+  list.pushFront(7);
+  list.pushFront(3);
+  list.pushFront(9);
+  list.pushFront(6);
+  list.pushFront(5);
+  list.pushFront(3);
+  std::cout << "List size: " << list.size() << std::endl;
 
-  std::cout << "Popped value: " << list->popFront()->value << std::endl;
-  std::cout << "List size: " << list->size() << std::endl;
-  list->print();
+  // std::cout << "Popped value: " << list->popFront()->value << std::endl;
+  // Doesn't delete the popped node from memory causing leak
+  // Bottom does and also is safe from getting a nullptr error if list is empty
+  Node *poppedNode = list.popFront();
+
+  if (poppedNode != nullptr) {
+    std::cout << "Popped value: " << poppedNode->value << std::endl;
+    delete poppedNode;
+  }
+
+  std::cout << "List size: " << list.size() << std::endl;
+  list.print();
 }
